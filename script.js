64d9c57d61a03436c0a23341a12c08795002382e
Abhay Chaturvedi <109450662+abhay707@users.noreply.github.com>
2026-08-22 17:39:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your JS code here. If required.
+let name = document.getElementById("username");
+let password = document.getElementById("password");
+let checkbox = document.getElementById("checkbox");
+let submitBtn = document.getElementById("submit");
+let existBtn = document.getElementById("existing");
+
+submitBtn.addEventListener('click', () =>{
+	let userName = name.value.trim();
+	let userPass = password.value.trim();
+
+	if(userName && userPass){
+		
+		if(!checkbox.checked){
+			alert(`Logged in as ${userName}`);
+			localStorage.clear();
+			existBtn.style.display = "none";
+		}else{
+			localStorage.setItem("name", userName);
+			localStorage.setItem("password", userPass);
+			existBtn.style.display = "block";
+		}
+		
+	}else{
+		alert("Fill all the fields");
+	}
+	name.value = "";
+	password.value = "";
+});
+
+existBtn.addEventListener('click', () =>{
+	let storedName = localStorage.getItem("name");
+	alert(`Login as existing user ${storedName}`);
+})
+
+
+
+
+
